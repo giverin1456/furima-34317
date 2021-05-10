f424d71eefce33b816f681bc71188a03462a34f5
giverin1456 <give.taiki1456@gmail.com>
2021-05-10 22:56:00 +0900

    ユーザー管理機能

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name,
+    format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/},
+    presence: true
+  validates :last_name,
+    format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/},
+    presence: true
+  validates :first_kana,
+    format: { with: /\A([ァ-ン]|ー)+\z/},
+    presence: true
+  validates :last_kana,
+    format: { with: /\A([ァ-ン]|ー)+\z/},
+    presence: true
+  validates :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    first_name            {'儀武'}
+    last_name             {'大樹'}
+    first_kana            {'ギブ'}
+    last_kana             {'タイキ'}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    birthday              {'1980-03-04'}
+  end
+end
