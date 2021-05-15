6f3484fabfdf2358b6247b655e0c8cba4f1ad429
giverin1456 <give.taiki1456@gmail.com>
2021-05-15 17:44:57 +0900

    商品出品機能　修正2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,27 +29,27 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include "Text can't be blank"
     end
     it 'category_idが1では登録できない' do
-      @item.category_id = '1'
+      @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include "Category must be other than 1"
     end
     it 'status_idが1では登録できない' do
-      @item.status_id = '1'
+      @item.status_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include "Status must be other than 1"
     end
     it 'delivery_charge_idが1では登録できない' do
-      @item.delivery_charge_id = '1'
+      @item.delivery_charge_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include "Delivery charge must be other than 1"
     end
     it 'prefecture_idが1では登録できない' do
-      @item.prefecture_id = '1'
+      @item.prefecture_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include "Prefecture must be other than 1"
     end
     it 'day_idが1では登録できない' do
-      @item.day_id = '1'
+      @item.day_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include "Day must be other than 1"
     end
