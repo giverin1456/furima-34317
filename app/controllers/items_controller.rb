cfc7986f54c5ef5bd40d640b9b4888561c6b0e3f
giverin1456 <give.taiki1456@gmail.com>
2021-05-16 11:34:35 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_index, only: [:edit]
+  
   def index
     @items = Item.all.order("created_at DESC")
 
@@ -24,8 +25,27 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(items_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
   def items_params
     params.require(:item).permit(:name, :text, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless Item.find(params[:id]).user.id == current_user.id
+      redirect_to action: :index
+    end
   end
 end
